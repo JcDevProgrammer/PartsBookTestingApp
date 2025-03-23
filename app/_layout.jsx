04492286e0381900c10c4cee43c7328bd9a8048a
John Carlo Canin <caninjc@gmail.com>
2025-03-23 21:43:50 +0800

    push new another codes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from "react";
+// app/_layout.jsx
 import { Stack } from "expo-router";
-import { Platform } from "react-native";
-import SplashScreen from "./SplashScreen";
+import React from "react";
 
 export default function RootLayout() {
-  const [isSplashVisible, setIsSplashVisible] = useState(Platform.OS !== "web");
-
-  useEffect(() => {
-    if (Platform.OS !== "web") {
-      const timer = setTimeout(() => {
-        setIsSplashVisible(false);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, []);
-
-  if (isSplashVisible) {
-    return <SplashScreen />;
-  }
-
-  return <Stack screenOptions={{ headerShown: false }}>{}</Stack>;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
